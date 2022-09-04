45f4313460c9149ffcdbebe3db84a88a2fbfa71e
HoangLong-Lumi <111274514+HoangLong-Lumi@users.noreply.github.com>
2022-09-04 22:15:28 +0700

    sdm632-common: Build vibrator HIDL

--- sdm632.mk
+++ sdm632.mk
@@ -160,6 +160,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk-sp
